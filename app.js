76d3dcf82db1944af3ee5f116c8808368ccfd22e
jordangunther <jordankgunther@gmail.com>
2015-10-06 20:14:29 -0600

    mini-paint complete... do over for more understanding

---
+++ app.js
@@ -0,0 +1,60 @@
+/* Step1 */
+$(document).ready(function() {
+	/* Step2 */
+	$('.box').on('click', function() {
+		$(this).addClass('white');
+	})
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass('white');
+	})
+	/* Step3 */
+	$('#reset').on('click', function() {
+		$('.box').removeClass('white')
+	})
+	/* Step4 */
+	var color = 'white'
+
+	 $('#red').on('click', function() {
+    	color = 'red';
+  	})
+
+	$('#blue').on('click', function() {
+    	color = 'blue';
+  	})
+
+  	$('#green').on('click', function() {
+    	color = 'green';
+  	})
+
+  	$('#yellow').on('click', function() {
+    	color = 'yellow';
+  	})
+
+  	$('#white').on('click', function() {
+    	color = 'white';
+  	})
+
+  	$('.box').on('click', function() {
+    	$(this).addClass(color);
+  	})
+
+  	$('.box').on('dblclick', function() {
+    	$(this).removeClass(color);
+  	})
+
+  	$('#reset').on('click', function() {
+    	$('.box').removeClass(color)
+  	})
+
+  	var colors = 'white green red blue yellow'
+
+  	$('.box').on('dblclick', function() {
+    	$(this).removeClass(colors);
+  	})
+
+  	$('#reset').on('click', function() {
+    	$('.box').removeClass(colors)
+  	})
+})
+	
